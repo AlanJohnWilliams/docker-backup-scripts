a871fd01cd89a448b88c3c69618d6379298ad78c
Alan Williams <root@nuc>
2021-01-25 15:14:31 -0700

    use docker-rsync instead of docker-backup

--- backup-volumes.sh
+++ backup-volumes.sh
@@ -2,12 +2,21 @@ for i in `docker inspect --format='{{.Name}}' $(docker ps -q) | cut -f2 -d\/`
         do container_name=$i
         mkdir -p $backup_path/$container_name
         echo -n "$container_name - "
+
+	#docker run --rm \
+  	#--volumes-from $container_name \
+  	#-v $backup_path:/backup \
+  	#-e TAR_OPTS="$tar_opts" \
+  	#alanjohnwilliams/docker-backup \
+        #backup "$container_name/$container_name-volume.tar.xz"
+
 	docker run --rm \
   	--volumes-from $container_name \
   	-v $backup_path:/backup \
-  	-e TAR_OPTS="$tar_opts" \
-  	alanjohnwilliams/docker-backup \
-        backup "$container_name/$container_name-volume.tar.xz"
+  	-e RSYNC_OPTS="$tar_opts" \
+  	alanjohnwilliams/docker-rsync \
+        backup "/backup/$container_name"
+
 	if [ $? -ne 0 ]; then
     		echo "Error"
         else
